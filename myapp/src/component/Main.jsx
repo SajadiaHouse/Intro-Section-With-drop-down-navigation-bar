afa863f1c61381b4afbd67cf4293b62eba33bed9
M Bilal <95403773+SajadiaHouse@users.noreply.github.com>
2023-10-29 23:33:18 +0500

    commit

--- myapp/src/component/Main.jsx
+++ myapp/src/component/Main.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const Main = () => {
   return (
     <>
-      <div className='mx-32 grid gap-16 grid-cols-2 w-auto'>
-        <div className='p-10'>
-          <h1 className='text-6xl font-bold'>Make remote work</h1>
+      <div className='md:mx-[200px] flex md:gap-[150px] md:flex-row flex-col-reverse w-auto'>
+        <div className='md:text-left text-center p-6 md:p-[30px] md:mt-8 mt-4'>
+          <h1 className='md:text-6xl text-4xl font-bold'>Make remote work</h1>
           <p className='text-md mt-10'>Get your team in sync. no matter your location. Streamlining processes, create team rituals, and watch productivity soar.</p>
-          <button className='p-2 border bg-black text-white hover:text-black hover:border-black hover:bg-white rounded-xl w-[120px] my-8'>Learn more</button>
-          <div className='grid gap-3 grid-cols-4 mt-6'>
+          <button className='p-2 border bg-black text-white hover:text-black hover:border-black hover:bg-white rounded-xl w-[120px] my-10'>Learn more</button>
+          <div className='grid gap-3 grid-cols-4 mt-6 md:mt-12'>
             <div>
               <img src="./images/client-databiz.svg" alt="" className='object-scale-down' />
             </div>
@@ -24,9 +24,9 @@ const Main = () => {
             </div>
           </div>
         </div>
-        <div >
-          <img src="./images/image-hero-desktop.png" className='h-[90vh] ml-auto' alt=""   />
-        </div>
+        
+          <img src="./images/image-hero-desktop.png" alt="" className='flex-1 md:h-[500px] md:ml-auto md:w-auto' />
+        
       </div>
     </>
   );
--- myapp/src/component/Navbar.jsx
+++ myapp/src/component/Navbar.jsx
@@ -3,19 +3,39 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <div className='flex flex-row h-16 w-full p-4 my-4'>
+    <div className='md:mx-[100px]'>
+      <div className='flex md:flex-row md:h-16 md:w-full p-4 md:my-4'>
         <img src="./images/logo.svg" className='object-scale-down' alt="" />
-        
-          <div className='w-24 align-middle text-center blok'><a href="" >Features</a></div>
-          <div className='w-24 align-middle text-center blok'><a href="" >Career</a></div>
-          <div className='w-24 align-middle text-center blok'><a href="" >Company</a></div>
-          <div className='w-24 align-middle text-center blok'><a href="" >About</a></div>
+          
+          <div className='group relative w-24 align-middle text-center blok  hover:font-semibold'><a href="">Features<img src="./images/icon-arrow-down.svg" alt="" className='inline-block ml-1 transition-transform transform group-hover:rotate-180' /></a>
+          <div className='bg-slate-100 shadow-2xl p-3 w-[120px] h-auto absolute invisible group-hover:visible z-50'>
+              <ul className='text-left text-md'>
+                <li className='inline-flex mb-1 align-middle hover:font-bold hover:bg-slate-200'><img src="./images/icon-todo.svg" alt="" className='w-3 h-3 mr-3 self-center ' />Todo List</li>
+                <li className='inline-flex mb-1 align-middle hover:font-bold hover:bg-slate-200'><img src="./images/icon-calendar.svg" alt="" className='w-3 h-3  self-center mr-3' />Calendar</li>
+                <li className='inline-flex mb-1 align-middle hover:font-bold hover:bg-slate-200'><img src="./images/icon-reminders.svg" alt="" className='w-3 h-3 self-center  mr-3' />Reminder</li>
+                <li className='inline-flex mb-1 align-middle hover:font-bold hover:bg-slate-200'><img src="./images/icon-planning.svg" alt="" className='w-3 h-3  self-center mr-3' />Planing</li>
+              </ul>
+          </div>
+          </div>
+          <div className='group w-24 align-middle text-center blok  hover:font-semibold'><a href="" >Career<img src="./images/icon-arrow-down.svg" alt="" className='inline-block ml-1 transition-transform transform group-hover:rotate-180' /></a>
+              <div className='absolute text-md invisible group-hover:visible bg-slate-100 shadow-2xl w-auto h-auto p-2'>
+                <ul className='text-left'>
+                  <li><a href="" className='mb-1'>History</a></li>
+                  <li><a href="" className='mb-1'>Our Team</a></li>
+                  <li><a href="" className='mb-1'>Blog</a></li>
+                </ul>
+              </div>
+          </div>
+          <div className='w-24 align-middle text-center blok  hover:font-semibold'><a href="" >Company</a></div>
+          <div className='w-24 align-middle text-center blok  hover:font-semibold'><a href="" >About</a></div>
         
         <div className='ml-auto'>
-          <a href=""  className='w-24 mr-10 align-middle text-center blok'>Login</a>
-          <a href="" className='border p-1.5 rounded-md w-[100px] border-slate-800 align-middle text-center'>Register</a>
+          <a href=""  className='w-24 mr-4 align-middle text-center blok  hover:font-semibold'>Login</a>
+          <a href="" className='border p-1.5 rounded-md w-[100px] border-slate-800 align-middle text-center hover:bg-black hover:text-white'>Register</a>
+        </div>
         </div>
       </div>
+      
     </>
   )
 }
